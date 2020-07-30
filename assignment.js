3aefdb91324f81592b4f535e7894569cd698015a
Sadia Akter <sadiaakterwork@gmail.com>
2020-07-30 23:24:01 +0600

    feet o mile

---
+++ assignment.js
@@ -0,0 +1,28 @@
+//Feet to Mile Convert
+function feetToMile(feet) {
+    const mile = feet / 5280;
+    return  mile;
+}
+var result = feetToMile(50000);
+console.log("Mile:", result);
+
+
+//Wood Calculator
+
+
+
+
+//Brick Calculator
+
+
+
+
+//Find Tiny Friend Name
+
+
+
+
+
+
+
+
